1b614cc6d1c24126da3d793565ed35b125e5f23a
Michael Copland <33166689+mcopland@users.noreply.github.com>
2021-01-02 20:48:41 -0600

    Add submissions for January 2nd.
    2021-01-02

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from models import TreeNode
 from typing import List
 
 
@@ -64,3 +65,45 @@ class Jan01:
             res += dic.get(i, [])
 
         return res == arr
+
+
+# getTargetCopy
+class Jan02:
+    def get_target_copy(self, original: TreeNode, cloned: TreeNode,
+                        target: TreeNode) -> TreeNode:
+        """Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+
+        Given two binary trees original and cloned and given a reference to a
+        node target in the original tree.
+
+        The cloned tree is a copy of the original tree.
+
+        Return a reference to the same node in the cloned tree.
+
+        Note that you are not allowed to change any of the two trees or the
+        target node and the answer must be a reference to a node in the cloned
+        tree.
+
+        Follow up: Solve the problem if repeated values on the tree are
+        allowed.
+
+        Constraints:
+        - The number of nodes in the tree is in the range [1, 10^4].
+        - The values of the nodes of the tree are unique.
+        - target node is a node from the original tree and is not null.
+        """
+        if not original:
+            return None
+
+        if original is target:
+            return cloned
+
+        left = self.get_target_copy(self, original.left, cloned.left, target)
+        if left:
+            return left
+
+        right = self.get_target_copy(self, original.right, cloned.right, target)
+        if right:
+            return right
+
+        return None
